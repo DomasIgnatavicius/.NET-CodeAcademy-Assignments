1066da19abe6870ddceca8854d1fa421abe4f586
domasIgnatavicius <ignatavicius.domas@gmail.com>
2022-06-19 22:33:24 +0300

    Termometras ir Kelias

---
+++ 03. Kelias/Program.cs
@@ -0,0 +1,75 @@
+﻿
+//PARAŠYTI PROGRAMĄ KURI PRAŠO ĮVESTI ATSTUMĄ (KILOMENTRAIS) TARP TAŠKŲ A IR B IR
+//DVIEJŲ TRANSPORTO PRIEMONIŲ GREITĮ (KM/H).
+//VIENA TRANSPORTO PRIEMONĖS PRADEDA VAŽIUOTI IŠ A, KITA IŠ B.STARTUOJA VIENU METU.
+
+Console.WriteLine("Iveskite atstuma(km) tarp A ir B");
+double atstumas = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Iveskite pirmos transporto priemones greiti, kuri keliaus is tasko A: ");
+double greitisA = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Iveskite antros transporto priemones greiti, kuri keliaus is tasko B: ");
+double greitisB = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine();
+
+
+// -PASKAIČIUOTI ATSTUMĄ NUO A IKI VIETOS KURIOJE TRASPORTO PRIEMONĖS SUTITIKS METRAIS.
+// -PASKAIČIUOTI LAIKĄ KADA TRASPORTO PRIEMONĖS SUSITIKS SEKUNDĖMIS.
+
+// Pagal proporcija apsiskaiciavau laika, kada transporto priemones susitiks, tada ta laika padauginau is greicioA
+// ir gavau kokiu atstumu susitiko nuo tasko A
+
+
+//PVZ: atstumas: 40, greitisA: 32km/h, greitisB: 64km/h
+
+// kartu per valanda nuvazious 240% viso kelio(96km)
+double kiekProcKelioNuvaziavoKartuSudejus = (greitisA + greitisB) * 100 / atstumas;
+
+// Kad nuvaziuotu 100% kelio reikes 0.4166666666666667h
+double kiekValanduReikesVisamKeliuiNuvaziuoti = 100 / kiekProcKelioNuvaziavoKartuSudejus;
+
+// automobilis A per 0.4166666666666667h nuvaziuos, 13.473684210526315km, tuo tarpu automobilis B nuvaziuos 26.666666666666668
+double susitikimoAtstumasRelativeToA = greitisA * kiekValanduReikesVisamKeliuiNuvaziuoti;
+double susitikimoAtstumasRelativeToB = greitisB * kiekValanduReikesVisamKeliuiNuvaziuoti;
+// Console.WriteLine(susitikimoAtstumasRelativeToB + susitikimoAtstumasRelativeToA == atstumas); TRUE
+
+double susitikimoAtstumasRelativeToAMetrais = susitikimoAtstumasRelativeToA * 1000;
+double kadaSusitiksSekundemis = kiekValanduReikesVisamKeliuiNuvaziuoti * (double)3600;
+
+Console.WriteLine($"Susitikimo atstumas nuo tasko A: {Math.Round(susitikimoAtstumasRelativeToAMetrais, 2)}km");
+Console.WriteLine($"Laikas po kurio susitiks transporto priemones: {kadaSusitiksSekundemis}s");
+
+Console.WriteLine();
+
+// - PASKAIČIUOTI LAIKĄ, KADA TRASPORTUO PRIEMONĖS PASIEKS GALIUTINIUS TAŠKUS MINUTĖMIS.
+
+double pasieksGalutiniTaskaAutomobilisAMinutemis = (atstumas / greitisA) * 60;
+double pasieksGalutiniTaskaAutomobilisBMinutemis = (atstumas / greitisB) * 60;
+
+Console.WriteLine($"Transporto priemone A galutini tiksla pasieks per {pasieksGalutiniTaskaAutomobilisAMinutemis} min");
+Console.WriteLine($"Transporto priemone B galutini tiksla pasieks per {pasieksGalutiniTaskaAutomobilisBMinutemis} min");
+Console.WriteLine();
+
+//- PASKAIČIUOTI KIEK GRAMŲ CO2 IŠSKYRĖ ABI TRASPORTO PIEMONĖS KARTU SUDĖJUS. CO2 NORMA YRA 95 g/km.
+
+double CO2isskirimas = atstumas * 2 * 95;
+
+Console.WriteLine($"Vaziuodamos transporto priemones kartu isskyre {CO2isskirimas} g CO2");
+
+Console.WriteLine();
+
+/*-GRAFIŠKAI PAVAIZDUOTI KELIĄ NUO A IKI B SUSKIRSTYTĄ Į 20 LYGIŲ SEGMENTŲ (TARKIME ĮVESTAS ATSTUMAS YRA 100KM, TUOMENT TURĖSIME 20 SEGMENTU PO 5KM).  
+    A) PARODYTI VISO KELIO ILGĮ KM
+    B) PARODYTI SEGMENTO ILGĮ KM
+    C) PARODYTI KURIAME SEGMENTE TRASPORTO PREMONĖS SUTIKS IR ATSTUMĄ IKI SUSITIKIMO (TAŠKAS V)
+    D) PARODYTI ABIEJŲ TRANSPORTO PRIEMONIŲ VAŽIAVIMO TRUKMĘ
+    if naudoti negalima, ternary operator (?) naudoti negalima, ciklų naudoti negalima*/
+
+int dvidesimtadalis = Convert.ToInt32(atstumas) / 20;
+int segmentoIlgis = Convert.ToInt32(susitikimoAtstumasRelativeToA);
+
+Console.WriteLine($"   {dvidesimtadalis}km  {dvidesimtadalis}km  {dvidesimtadalis}km  {dvidesimtadalis}km  {dvidesimtadalis}km  {dvidesimtadalis}km  {dvidesimtadalis}km  {dvidesimtadalis}km  {dvidesimtadalis}km  {dvidesimtadalis}km  {dvidesimtadalis}km  {dvidesimtadalis}km  {dvidesimtadalis}km  {dvidesimtadalis}km  {dvidesimtadalis}km  {dvidesimtadalis}km  {dvidesimtadalis}km  {dvidesimtadalis}km  {dvidesimtadalis}km  {dvidesimtadalis}km");
+Console.WriteLine($"A|____|____|____|____|____|____|____|____|____|____|____|____|____|____|____|____|____|____|____|____|");
+Console.WriteLine($"|-----------------------------------------{atstumas}km-------------------------------------------------------|");
---
+++ Termometras/Program.cs
@@ -0,0 +1,75 @@
+﻿
+// 1. Paprašykite naudotojo įvesti 1 skaičių - temperatūrą pagal Celsijų.
+
+Console.WriteLine("Iveskite temperatura(celsijus): ");
+double tempCelsijus = double.Parse(Console.ReadLine());
+
+// Temperatura pagal farenheita ir kelvina
+
+double tempFarenheitais = Math.Round(tempCelsijus * 1.8 + 32, 2);
+Console.WriteLine($"Farenheitais: {tempFarenheitais}F");
+
+double tempKelvinais = Math.Round( tempCelsijus + 273.15, 2);
+Console.WriteLine($"Kelvinais: {tempKelvinais}K");
+
+Console.WriteLine();
+
+// - Gautą temperatūrą pagal farenheitą perskaičiuokite į Celsijų
+// ir patikrinkite ar sutampa su įvestu skaičių (išveskite true/false)
+// - Gautą temperatūrą pagal kelviną perskaičiuokite į celsijų
+// ir patikrinkite ar sutampa su įvestu skaičiu (išveskite true/false)
+// - Paskaičiuotą temperatūrą pagal farenheita peverskite į kelviną
+//   ir patikrinkite ar sutampa su ankstesniais skaičiavimais (išveskite true/false)
+
+double FtoC = Math.Round( (tempFarenheitais - 32) / 1.8, 2); // farenheitai i celsijus
+Console.WriteLine($"{tempFarenheitais}F yra {FtoC}C {tempCelsijus == FtoC}");
+
+double KtoC =  Math.Round(tempKelvinais - 273.15, 2); // Kelvinai i celsijus
+Console.WriteLine($"{tempKelvinais}K yra {KtoC}C {tempCelsijus == KtoC}");
+
+double FtoK = Math.Round((tempFarenheitais + 459.67) * 5/9, 2); // Fenrenheitai i kelvinus
+Console.WriteLine($"{tempFarenheitais}F yra {FtoK}K {tempKelvinais == FtoK}");
+
+Console.WriteLine();
+
+//- Nupieškite termometrą pagal Celsijų
+
+// a) Atvaizduokite skalę, sugraduotą kas 5 laipsnius C priklausomai nuo įvestos temperatūros pridedant
+// ir atimant 40 laipsnių (tarkime įvesta buvo 10, tuomet skalė bus nuo -30 iki +50)
+
+int virsausAmpliture = Convert.ToInt32(tempCelsijus) + 40;
+int apaciosAmplitude = Convert.ToInt32(tempCelsijus) - 40;
+Console.WriteLine(virsausAmpliture);
+Console.WriteLine(apaciosAmplitude);
+
+Console.WriteLine();
+
+Console.WriteLine("|-------------------|");
+Console.WriteLine("|   ^C         ^F");
+
+Console.WriteLine($"| {tempCelsijus+40,4} - | | - {Math.Round(tempCelsijus+40) * 1.8 + 32, -4} |");
+Console.WriteLine($"| {tempCelsijus+35,4} - | | - {convertCtoF(tempCelsijus + 35),-4} |");
+Console.WriteLine($"| {tempCelsijus+30,4} - | | - {convertCtoF(tempCelsijus + 30),-4} |");
+Console.WriteLine($"| {tempCelsijus+25,4} - | | - {convertCtoF(tempCelsijus + 25),-4} |");
+Console.WriteLine($"| {tempCelsijus+20,4} - | | - {convertCtoF(tempCelsijus + 20),-4} |");
+Console.WriteLine($"| {tempCelsijus+15,4} - | | - {convertCtoF(tempCelsijus + 15),-4} |");
+Console.WriteLine($"| {tempCelsijus+10,4} - | | - {convertCtoF(tempCelsijus + 10),-4} |");
+Console.WriteLine($"| {tempCelsijus+5,4} - | | - {convertCtoF(tempCelsijus + 5),-4} |");
+Console.WriteLine($"| {tempCelsijus,4} - |#| - {convertCtoF(tempCelsijus),-4} |"); // vidurio taskas
+Console.WriteLine($"| {tempCelsijus-5,4} - |#| - {convertCtoF(tempCelsijus - 5),-4} |");
+Console.WriteLine($"| {tempCelsijus-10,4} - |#| - {convertCtoF(tempCelsijus - 10),-4} |");
+Console.WriteLine($"| {tempCelsijus-15,4} - |#| - {convertCtoF(tempCelsijus - 15),-4} |");
+Console.WriteLine($"| {tempCelsijus-20,4} - |#| - {convertCtoF(tempCelsijus - 20),-4} |");
+Console.WriteLine($"| {tempCelsijus-25,4} - |#| - {convertCtoF(tempCelsijus - 25),-4} |");
+Console.WriteLine($"| {tempCelsijus-30,4} - |#| - {convertCtoF(tempCelsijus - 30),-4} |");
+Console.WriteLine($"| {tempCelsijus-35,4} - |#| - {convertCtoF(tempCelsijus - 35),-4} |");
+Console.WriteLine($"| {tempCelsijus-40,4} - |#| - {convertCtoF(tempCelsijus - 40),-4} |");
+Console.WriteLine("|-------------------|");
+
+
+
+double convertCtoF(double C)
+{
+    double converted = Math.Round(C * 1.8 + 32, 2);
+    return converted;
+}
