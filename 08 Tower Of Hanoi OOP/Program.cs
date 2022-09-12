0e7c819fe35f471f14cd3347cc3ee6e055d4df80
domasIgnatavicius <ignatavicius.domas@gmail.com>
2022-09-12 15:19:52 +0300

    pilna programa ir logginimas(txt ir csv)

--- 08 Tower Of Hanoi OOP/Program.cs
+++ 08 Tower Of Hanoi OOP/Program.cs
@@ -128,7 +128,7 @@ namespace _08_Tower_Of_Hanoi_OOP
                 }
                 
             }
-            ejimoNr++;
+            
             
         }
 
@@ -139,6 +139,9 @@ namespace _08_Tower_Of_Hanoi_OOP
                 if (Tower[pasirinktasStulpelis - 1].eilutes[i] == "     |     ")
                 {
                     Tower[pasirinktasStulpelis - 1].eilutes[i] = aktyvusDiskas.piesinukas;
+                    ejimoNr++;
+                    moveLog(pasirinktasStulpelis);
+
                     aktyvusDiskas = new Diskas();
                     gameState = "disko paemimas";
                     break;
@@ -152,6 +155,8 @@ namespace _08_Tower_Of_Hanoi_OOP
                             if(aktyvusDiskas.keliuDaliu < Diskai[j].keliuDaliu)
                             {
                                 Tower[pasirinktasStulpelis - 1].eilutes[i - 1] = aktyvusDiskas.piesinukas;
+                                ejimoNr++;
+                                moveLog(pasirinktasStulpelis);
                                 aktyvusDiskas = new Diskas();
                                 gameState = "disko paemimas";
                                 break;
@@ -208,6 +213,80 @@ namespace _08_Tower_Of_Hanoi_OOP
                 }
             }
             gameState = "disko padejimas";
+        }
+
+        public static void moveLog(int pasirinktasStulpelis)
+        {
+            string stulpelisIsKurioPaeme = "";
+            string stulpelisIKuriPadejo = "";
+            var logBuilder = new StringBuilder();
+            for (int i = 0; i < 4; i++)
+            {
+                if (i + 1 == aktyvusDiskas.keliuDaliu)
+                {
+                    stulpelisIsKurioPaeme = diskuVietos[i].ToString();
+                    stulpelisIsKurioPaeme = stulpelioNrToString(pasirinktasStulpelis.ToString(), stulpelisIsKurioPaeme, ref stulpelisIKuriPadejo);
+                    logBuilder.Append(pasirinktasStulpelis.ToString());
+                }
+                else
+                    logBuilder.Append(diskuVietos[i]);
+            }
+            diskuVietos = logBuilder.ToString();
+
+            //CSV
+            string pathCsv = @"C:\Users\ignat\Documents\.NET CodeAcademy\Assignments\NET CodeAcademy Assignments\08 Tower Of Hamoi OOP.Domain\Logs\log.csv";
+            TextWriter csv = new StreamWriter(pathCsv, true);
+            var date = new DateTime();
+            date = DateTime.Now;
+            var newLineCSV = $"{date.ToString("yyyy-MM-dd hh:mm")},{ejimoNr},{diskuVietos[0]},{diskuVietos[1]},{diskuVietos[2]},{diskuVietos[3]}";
+            csv.WriteLine(newLineCSV);
+            csv.Close();
+
+            //TXT
+            string pathTxt = @"C:\Users\ignat\Documents\.NET CodeAcademy\Assignments\NET CodeAcademy Assignments\08 Tower Of Hamoi OOP.Domain\Logs\log.txt";
+            TextWriter txt = new StreamWriter(pathTxt, true);
+            var newLineTXT = $"zaidime, kuris pradetas {date.ToString("yyyy-MM-dd hh:mm")}, ejimu nr {ejimoNr} {aktyvusDiskas.keliuDaliu} daliu diskas buvo paimtas is {stulpelisIsKurioPaeme} stulpelio ir padetas i {stulpelisIKuriPadejo} stulpeli";
+            txt.WriteLine(newLineTXT);
+            txt.Close();
+        }
+
+        public static string stulpelioNrToString(string pasirinktasStulpelis, string stulpelisIsKurioPaeme, ref string stulpelisIKuriPadejo)
+        {
+            switch (stulpelisIsKurioPaeme)
+            {
+                case "1":
+                    stulpelisIsKurioPaeme = "pirmo";
+                    break;
+                case "2":
+                    stulpelisIsKurioPaeme = "antro";
+                    break;
+                case "3":
+                    stulpelisIsKurioPaeme = "trecio";
+                    break;
+                case "4":
+                    stulpelisIsKurioPaeme = "ketvirto";
+                    break;
+                default:
+                    break;
+            }
+            switch (pasirinktasStulpelis)
+            {
+                case "1":
+                    stulpelisIKuriPadejo = "pirma";
+                    break;
+                case "2":
+                    stulpelisIKuriPadejo = "antra";
+                    break;
+                case "3":
+                    stulpelisIKuriPadejo = "trecia";
+                    break;
+                case "4":
+                    stulpelisIKuriPadejo = "ketvirta";
+                    break;
+                default:
+                    break;
+            }
+            return stulpelisIsKurioPaeme;
         }
     }
 }
